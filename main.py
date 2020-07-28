9bb43c5f325649e1061f67b0dc6ece7420354e00
Anne S <anne@users.noreply.github.com>
2020-07-27 23:48:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def download_youtube_video(youtube_url_passed_in):
     for file in dirs:
         file_list.append(str(file))
     print(file_list)
+    # download youtube video
     youtube_url = "https://www.youtube.com/watch?v=ZrsYIthVY-Y"
     youtube_url_array = [youtube_url]
     ydl_opts = {
@@ -35,11 +36,18 @@ def download_youtube_video(youtube_url_passed_in):
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download(youtube_url_array)
+    # get new file list
     dirs = os.listdir(".")
     new_file_list = []
     for file in dirs:
         new_file_list.append(str(file))
     print(new_file_list)
+    # remove files that were in the list previously
+    for old_item in file_list:
+        new_file_list.remove(old_item)
+    # now iterate through the new files list
+    for new_file in new_file_list:
+        os.system("gsutil cp " + str(new_file) + " gs://farmtwitter.appspot.com/")
     
 
     
